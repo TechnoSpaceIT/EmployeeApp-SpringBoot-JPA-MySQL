45e95ad6030a69b20ae0f7aef63fa35da253627f
TechnoSpaceIT <technospaceitclasses@gmail.com>
2023-07-07 21:16:08 +0530

    [ADDED]:[InfoGalaxy] Home API Implemented in Controller Layer

---
+++ src/main/java/com/infogalaxy/employeeapp/controller/EmployeeController.java
@@ -0,0 +1,17 @@
+package com.infogalaxy.employeeapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employeeapp")
+public class EmployeeController {
+
+	@GetMapping("/home")
+	public String home() {
+		return "Welcome to Employee App";
+	}
+	
+	
+}
